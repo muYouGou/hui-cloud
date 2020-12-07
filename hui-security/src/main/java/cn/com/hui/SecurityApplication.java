95ee10265ab1d137c23219548731c43fc1c5e885
guop <goupenn@126.com>
2020-12-07 22:54:47 +0800

    add new SecurityApplication

--- hui-security/src/main/java/cn/com/hui/SecurityApplication.java
+++ hui-security/src/main/java/cn/com/hui/SecurityApplication.java
@@ -1,18 +1,20 @@
-package cn.com.hui.common;
+package cn.com.hui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * @author goupe
- * @info common工具类
+ * @author guope
+ * @version V1.0
+ * @description: 启动类
+ * @date 2020-12-07 22:08
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class CommonRunApplication {
+public class SecurityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommonRunApplication.class, args);
+        SpringApplication.run(SecurityApplication.class, args);
     }
 }
