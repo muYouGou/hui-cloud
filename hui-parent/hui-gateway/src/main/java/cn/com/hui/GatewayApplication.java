2563647cda2647b9efa34062c137034ad1c2a355
guop <goupenn@126.com>
2020-12-11 23:27:14 +0800

    add gateway service

---
+++ hui-parent/hui-gateway/src/main/java/cn/com/hui/GatewayApplication.java
@@ -0,0 +1,20 @@
+package cn.com.hui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author guope
+ * @version V1.0
+ * @description: 网管启动类
+ * @date 2020-12-11 23:22
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
